9e906527538684a41bd64400387d2ce73e71ae7a
jonswedberg <jon.swedberg@hotmail.se>
2022-02-02 10:49:37 +0100

    auto commit

--- src/main/scala/util/FileHandler.scala
+++ src/main/scala/util/FileHandler.scala
@@ -6,12 +6,15 @@ import java.nio.charset.StandardCharsets.UTF_8
 object FileHandler:
     
     def write(fileName: String, output: String, path: String = "output/"): Unit = {
-        val finalFileName = 
+        val fileNameWithPath = path + {
             if fileName.endsWith(".txt") then 
                 fileName
             else fileName + ".txt"
-
-        val fileNameWithPath = path + finalFileName
+        }
     
         Files.write(Paths.get(fileNameWithPath), output.getBytes(UTF_8))
+    }
+
+    def read(fileName: String, path: String = "output/"): String = {
+        fileNameWithPath = path
     }
